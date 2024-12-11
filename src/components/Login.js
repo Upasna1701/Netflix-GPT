95bbf543776da3c86258bf88c1a5572b6a83a714
Upasna <upasna.r@autowoven.com>
2024-12-11 09:28:40 +0530

    form validation

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,18 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import Header from './Header'
+import { checkValidateData } from '../utils/validation'
 
 const Login = () => {
   const[isSignInForm,setIsSignInForm] = useState(true)
+  const[errorMessage,setErrorMessage] = useState(null)
+
+  const email = useRef(null);
+  const password = useRef(null);
+
+  const handleButtonClick = () =>{
+    const message = checkValidateData(email.current.value,password.current.value);
+    setErrorMessage(message)
+  }
 
   const toggleSignInForm = () => {
     setIsSignInForm(!isSignInForm)
@@ -16,13 +26,18 @@ const Login = () => {
 
       <div className='login-form absolute bg-[#000000b3] p-8 mx-auto left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 w-3/12'>
         <h2 className='text-white text-3xl font-semibold'>{isSignInForm ? 'Sign In' : 'Sign Up'}</h2>
-        <form>
+        <form onSubmit={(e) => e.preventDefault()}>
           {!isSignInForm &&
-            <input type="text" placeholder='Full Name' className='p-4 my-4 bg-transparent border-2 border-solid border-white rounded-2 w-full'/>
+            <input type="text" placeholder='Full Name' className='p-4 my-4 bg-transparent border-2 border-solid border-white rounded-2 w-full '/>
           }
-          <input type="email" placeholder='Email Address' className='p-4 my-4 bg-transparent border-2 border-solid border-white rounded-2 w-full'/>
-          <input type="password" placeholder='Password' className='p-4 my-4 bg-transparent border-2 border-solid border-white rounded-2 w-full'/>
-          <button className='p-4 my-6 bg-red-700 w-full rounded-lg text-white'>{isSignInForm ? 'Sign In' : 'Sign Up'}</button>
+          <input 
+          ref={email}
+          type="email" placeholder='Email Address' className='p-4 my-4 bg-transparent border-2 border-solid border-white rounded-2 w-full text-white'/>
+          <input 
+          ref={password}
+          type="password" placeholder='Password' className='p-4 my-4 bg-transparent border-2 border-solid border-white rounded-2 w-full text-white'/>
+          <p className='text-red-500 font-bold text-lg py-2'>{errorMessage}</p>
+          <button className='p-4 my-6 bg-red-700 w-full rounded-lg text-white' onClick={handleButtonClick}>{isSignInForm ? 'Sign In' : 'Sign Up'}</button>
         </form>
 
         <p className='cursor-pointer my-3 text-white text-xl' onClick={toggleSignInForm}>
